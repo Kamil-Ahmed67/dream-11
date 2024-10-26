3f9205f61f3c2985dbc4eabdf6b2fd9ceac658f0
Kamil-Ahmed67 <nabilahmedtalukder6272@gmail.com>
2024-10-26 17:02:37 +0600

    Available and Selected Players Toggle Functionalities Added

--- src/components/ActiveButtons/ActiveButtons.jsx
+++ src/components/ActiveButtons/ActiveButtons.jsx
@@ -1,31 +1,36 @@
 import PropTypes from "prop-types";
 
-const ActiveButtons = ({chosenPlayers,handleIsActiveState,isActive}) => {
+const ActiveButtons = ({ chosenPlayers, handleIsActiveState, isActive }) => {
     return (
         <div className="w-11/12 mx-auto flex justify-between items-center">
-        <div>
-            <h3 className="text-lg md:text-2xl text-gray-800 font-semibold md:font-bold">Available Players</h3>
+            <div>
+                <h3 className={`${isActive.card ? 'text-lg md:text-2xl text-gray-800 font-semibold md:font-bold' : 'hidden'}`}>
+                    Available Players
+                </h3>
+                <h3 className={`${!isActive.card ? 'text-lg md:text-2xl text-gray-800 font-semibold md:font-bold' : 'hidden'}`}>
+                    Selected Players <span>({chosenPlayers.length}/6)</span>
+                </h3>
+            </div>
+            <div className="flex">
+                <button
+                    onClick={() => handleIsActiveState("playerCard")}
+                    className={`${isActive.card ? 'bg-yellow-400 text-black' : 'bg-gray-100 text-gray-400'} py-2 px-4 rounded-l-lg`}
+                >
+                    Available
+                </button>
+                <button
+                    onClick={() => handleIsActiveState("selectedPlayer")}
+                    className={`${!isActive.card ? 'bg-yellow-400 text-black' : 'bg-gray-100 text-gray-400'} py-2 px-4 rounded-r-lg`}
+                >
+                    Selected ({chosenPlayers.length})
+                </button>
+            </div>
         </div>
-        <div className="flex">
-            <button
-                onClick={() => handleIsActiveState("playerCard")}
-                className={`${isActive.card ? 'bg-yellow-400 text-black' : 'bg-gray-100 text-gray-400'} py-2 px-4 rounded-l-lg`}
-            >
-                Available
-            </button>
-            <button
-                onClick={() => handleIsActiveState("selectedPlayer")}
-                className={`${!isActive.card ? 'bg-yellow-400 text-black' : 'bg-gray-100 text-gray-400'} py-2 px-4 rounded-r-lg`}
-            >
-                Selected ({chosenPlayers.length})
-            </button>
-        </div>
-    </div>
 
     );
 };
-ActiveButtons.propTypes={
-    chosenPlayers: PropTypes.array.isRequired, 
+ActiveButtons.propTypes = {
+    chosenPlayers: PropTypes.array.isRequired,
     handleIsActiveState: PropTypes.func.isRequired,
     isActive: PropTypes.object.isRequired,
 }
